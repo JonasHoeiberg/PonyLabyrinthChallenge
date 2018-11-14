82cd85d020b4fa19cd3ae5bc29e987cf03ceff57
JonasHoeiberg <jonashoeiberg@yahoo.dk>
2018-11-14 21:03:54 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ponylabyrinth/precache-manifest.1e3d0f400a43a7071f5b3702a4c2f0b5.js"
+  "/precache-manifest.5bd2895e37124f5f5d9a78c084e94db1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ponylabyrinth/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
