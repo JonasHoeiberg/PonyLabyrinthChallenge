77fbfb76c42486d9f8577a4bb4d7291d4642a436
JonasHoeiberg <jonashoeiberg@yahoo.dk>
2018-11-14 21:08:09 +0100

    Updates

---
+++ precache-manifest.7c1c6e66a9139994374b1ae6f187d316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a3fccf7e57de6baa153",
+    "url": "/PonyLabyrinthChallenge/static/js/runtime~main.0a3fccf7.js"
+  },
+  {
+    "revision": "11afb49d1a5dbc8c8d25",
+    "url": "/PonyLabyrinthChallenge/static/js/main.11afb49d.chunk.js"
+  },
+  {
+    "revision": "702f419e2a73196afac3",
+    "url": "/PonyLabyrinthChallenge/static/js/1.702f419e.chunk.js"
+  },
+  {
+    "revision": "11afb49d1a5dbc8c8d25",
+    "url": "/PonyLabyrinthChallenge/static/css/main.b50077b9.chunk.css"
+  },
+  {
+    "revision": "17bafe8e2e84c937af257570528b579c",
+    "url": "/PonyLabyrinthChallenge/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bd2895e37124f5f5d9a78c084e94db1.js"
+  "/PonyLabyrinthChallenge/precache-manifest.7c1c6e66a9139994374b1ae6f187d316.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PonyLabyrinthChallenge/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
