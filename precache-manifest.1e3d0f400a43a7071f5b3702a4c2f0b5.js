04f39630fca16be21472cc8648917ccc7f05ba48
JonasHoeiberg <jonashoeiberg@yahoo.dk>
2018-11-14 21:00:30 +0100

    Updates

---
+++ precache-manifest.1e3d0f400a43a7071f5b3702a4c2f0b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bbca9cc03e71a81b025",
+    "url": "/ponylabyrinth/static/js/runtime~main.8bbca9cc.js"
+  },
+  {
+    "revision": "bd4d85dc9171bd130ab2",
+    "url": "/ponylabyrinth/static/js/main.bd4d85dc.chunk.js"
+  },
+  {
+    "revision": "702f419e2a73196afac3",
+    "url": "/ponylabyrinth/static/js/1.702f419e.chunk.js"
+  },
+  {
+    "revision": "bd4d85dc9171bd130ab2",
+    "url": "/ponylabyrinth/static/css/main.b50077b9.chunk.css"
+  },
+  {
+    "revision": "43afccac098411ac628827966f7db2c1",
+    "url": "/ponylabyrinth/index.html"
+  }
+];
